40a20f8608cfc43e5c8b49751d21632741be229d
sai0698 <sairam.kotha@icloud.com>
2022-07-14 00:12:01 +0530

    screen 3 added

---
+++ app/src/main/java/com/ksappdev/swoosh/Screen3.kt
@@ -0,0 +1,11 @@
+package com.ksappdev.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Screen3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen3)
+    }
+}
