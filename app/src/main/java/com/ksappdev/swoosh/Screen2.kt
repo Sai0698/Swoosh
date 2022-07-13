1b4ef837a92f8d359729762240e80f393ddd099d
sai0698 <sairam.kotha@icloud.com>
2022-07-14 01:00:07 +0530

    Buttons functioning

--- app/src/main/java/com/ksappdev/swoosh/Screen2.kt
+++ app/src/main/java/com/ksappdev/swoosh/Screen2.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_screen2.*
 
 class Screen2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,32 @@ class Screen2 : AppCompatActivity() {
         setContentView(R.layout.activity_screen2)
     }
 
-    fun secondbuttonclicked(view: View) {
-        val intent = Intent(this,Screen3::class.java)
-        startActivity(intent)
+    var selectedstring= ""
 
+    fun selectedco_ed(view: View) {
+        mensBtn.isChecked =false
+        womensBtn.isChecked=false
+        selectedstring = "co-ed"
+    }
+    fun selectedwomens(view: View) {
+        mensBtn.isChecked =false
+        coedBtn.isChecked=false
+
+        selectedstring = "womens"
+    }
+    fun selectedmens(view: View) {
+
+        coedBtn.isChecked =false
+        womensBtn.isChecked=false
+        selectedstring = "mens"
+    }
+    fun secondbuttonclicked(view: View) {
+        if(selectedstring!="") {
+            val intent = Intent(this, Screen3::class.java)
+            intent.putExtra(EXTRA_LEAGUE,selectedstring)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"Kindly select one League",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/ksappdev/swoosh/Screen3.kt
+++ app/src/main/java/com/ksappdev/swoosh/Screen3.kt
@@ -2,10 +2,16 @@ package com.ksappdev.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class Screen3 : AppCompatActivity() {
+
+//    var league = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen3)
+//        league = intent.getStringExtra(EXTRA_LEAGUE)
+
+//        Toast.makeText(this,league,Toast.LENGTH_LONG).show()
     }
 }
