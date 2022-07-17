4fc68da3545ecfb13ed253342962337dc93604d9
sai0698 <sairam.kotha@icloud.com>
2022-07-17 18:40:24 +0530

    Added Parcelable

--- app/src/main/java/com/ksappdev/swoosh/controllers/Screen2.kt
+++ app/src/main/java/com/ksappdev/swoosh/controllers/Screen2.kt
@@ -1,43 +1,43 @@
 package com.ksappdev.swoosh.controllers
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.ksappdev.swoosh.EXTRA_LEAGUE
+import com.ksappdev.swoosh.EXTRA_PLAYER
 import com.ksappdev.swoosh.R
+import com.ksappdev.swoosh.player.Player
 import kotlinx.android.synthetic.main.activity_screen2.*
 
-class Screen2 : AppCompatActivity() {
+class Screen2 : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen2)
     }
 
-    var selectedstring= ""
+    var player=Player("","")
 
     fun selectedco_ed(view: View) {
         mensBtn.isChecked =false
         womensBtn.isChecked=false
-        selectedstring = "co-ed"
+        player.league = "co-ed"
     }
     fun selectedwomens(view: View) {
         mensBtn.isChecked =false
         coedBtn.isChecked=false
 
-        selectedstring = "womens"
+        player.league = "womens"
     }
     fun selectedmens(view: View) {
 
         coedBtn.isChecked =false
         womensBtn.isChecked=false
-        selectedstring = "mens"
+        player.league = "mens"
     }
     fun secondbuttonclicked(view: View) {
-        if(selectedstring!="") {
+        if(player.league!="") {
             val skillintent = Intent(this, Screen3::class.java)
-            skillintent.putExtra(EXTRA_LEAGUE,selectedstring)
+            skillintent.putExtra(EXTRA_PLAYER,player)
             startActivity(skillintent)
         }else{
             Toast.makeText(this,"Kindly select one League",Toast.LENGTH_SHORT).show()
